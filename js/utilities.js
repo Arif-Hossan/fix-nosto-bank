df4cb3932705e9e757073c6a025b1335259366cf
Arif Hossan <arif.hossan.eu@gmail.com>
2023-03-06 22:45:08 +0600

    Link up all js file

---
+++ js/utilities.js
@@ -0,0 +1,19 @@
+export function getInputFieldValueById(inputFieldId) {
+  const inputField = document.getElementById(inputFieldId);
+  const inputFieldValueString = inputField.value;
+  const inputFieldValue = parseFloat(inputFieldValueString);
+  return inputFieldValue;
+  inputField.value = "";
+}
+
+export function getTextElementValueById(elementId) {
+  const textElement = document.getElementById(elementId);
+  const textElementValueString = textElement.innerText;
+  const textElementValue = textElementValueString;
+  textElementValue;
+}
+
+export function setTextElementValueById(elementId, newValue) {
+  const textElement = document.getElementById(elementId);
+  textElement.value = newValue;
+}
